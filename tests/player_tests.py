8335f85532fa8e743db8065ed11d50a75349b616
ariangokhale <55399896+ariangokhale@users.noreply.github.com>
2021-11-08 23:13:36 -0500

    Updated player tests

---
+++ tests/player_tests.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 15:01:28 2021
+
+@author: renem
+"""
+from dstruct_v2.player import Player
+
+import pytest
+
+@pytest.fixture
+def player():
+    return Player(100, "Michael_Scott")
+
+def test_constructor():
+    p = Player(100, "Michael_Scott")
+    assert isinstance(p, Player), "Constructor did not create Hand object"
+
+def test_add_chips(player):
+    assert player.add_chips(50) == 150, "Incorrect amount added"
+    
+    
+def test_subtract_chips(player):
+    assert player.subtract_chips(50) == 50, "50 chips were not subtracted"
+    with pytest.raises(AssertionError):
+        player.subtract_chips(200)
+    
+
+    
+    
+
+    
+    
